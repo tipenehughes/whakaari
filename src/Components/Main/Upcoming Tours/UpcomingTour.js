1d7318c5cf3c65cae295b57813ee0ec2ad15bb22
Tipene Hughes <tipenehughes@gmail.com>
2021-01-01 13:49:16 -0500

    Added window size event listener to tour cards. Set resopnsiveness for tour card animations

--- src/Components/Main/Upcoming Tours/UpcomingTour.js
+++ src/Components/Main/Upcoming Tours/UpcomingTour.js
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import TourCards from "./TourCards";
 
 import language from "../../../Languages/languages.json";
-
-import czech from "../../../Assets/img/flags/czech.svg";
-import austria from "../../../Assets/img/flags/austria.svg";
-import german from "../../../Assets/img/flags/german.svg";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
@@ -15,6 +11,18 @@ import styles from "../../../CSS/Main/Upcoming/UpcomingTour.module.css";
 
 const UpcomingTour = ({ lang }) => {
     const [count, setCount] = useState(0);
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const handleSetWidth = () => {
+        setWidth(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleSetWidth);
+    }, [width]);
+
+    const tablet = width < 769;
+    const mobile = width < 480;
 
     const handleSetCountUp = () => {
         return count < 5 ? setCount(count + 1) : null;
@@ -23,7 +31,9 @@ const UpcomingTour = ({ lang }) => {
         return count > 0 ? setCount(count - 1) : null;
     };
 
-    const translate = [75, 45, 15, -15, -45, -75];
+    const translateDesktop = [75, 45, 15, -15, -45, -75];
+    const translateTablet = [125, 75, 25, -25, -75, -125];
+    const translateMobile = [225, 135, 45, -45, -135, -225];
 
     const cards = [
         language[lang].tours.card.one,
@@ -44,7 +54,15 @@ const UpcomingTour = ({ lang }) => {
             </div>
             <div
                 className={styles.tourCards}
-                style={{ transform: `translateX(${translate[count]}vw)` }}
+                style={{
+                    transform: `translateX(${
+                        mobile
+                            ? translateMobile[count]
+                            : tablet
+                            ? translateTablet[count]
+                            : translateDesktop[count]
+                    }vw)`,
+                }}
             >
                 {cards.map((card, i) => {
                     return (
